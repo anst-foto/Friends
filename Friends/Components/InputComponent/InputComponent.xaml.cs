6ede2ba7acd2577ca401c1bfd7a72aa0dcd1f36c
Starinin Andrey <starininandrey@gmail.com>
2023-12-16 17:31:36 +0300

    Add InputComponent

---
+++ Friends/Components/InputComponent/InputComponent.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace Friends.Components.InputComponent;
+
+public partial class InputComponent : UserControl
+{
+    public InputComponent(string labelName, string inputText, bool isEnabled)
+    {
+        InitializeComponent();
+
+        DataContext = this;
+
+        Label.Content = labelName;
+
+        Input.Text = inputText;
+        Input.IsEnabled = isEnabled;
+    }
+}

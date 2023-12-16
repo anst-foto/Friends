59c9da877d066c352dfd371c3f83611e209e505b
Starinin Andrey <starininandrey@gmail.com>
2023-12-16 17:31:49 +0300

    Add FriendComponent

---
+++ Friends/Components/FriendComponent/FriendComponent.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+using Friends.BL.Models;
+using Friends.Windows.FriendWindow;
+
+namespace Friends.Components.FriendComponent;
+
+public partial class FriendComponent : UserControl
+{
+    public FriendComponent(Friend friend)
+    {
+        InitializeComponent();
+        DataContext = this;
+
+        LinkUserName.Click += (sender, args) =>
+        {
+            new FriendWindow().Show();
+        }; 
+        UserName.Text = friend.Username;
+
+        var nameInputComponent = new InputComponent.InputComponent(
+            labelName: "Name",
+            inputText: friend.Name,
+            isEnabled: false)
+        {
+            Width = 300
+        };
+        var companyInputComponent = new InputComponent.InputComponent(
+            labelName: "Company",
+            inputText: friend.Company.Name,
+            isEnabled: false)
+        {
+            Width = 300
+        };
+
+        Panel.Children.Add(nameInputComponent);
+        Panel.Children.Add(companyInputComponent);
+    }
+}

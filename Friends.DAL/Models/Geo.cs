0ad46fbfc1cb2b1bf89da994bc9eb27660242239
Starinin Andrey <starininandrey@gmail.com>
2023-12-16 14:46:36 +0300

    Add DAL

---
+++ Friends.DAL/Models/Geo.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Friends.DAL.Models;
+
+public class Geo
+{
+    [JsonPropertyName("lat")]
+    public string Lat { get; set; }
+
+    [JsonPropertyName("lng")]
+    public string Lng { get; set; }
+}
---
+++ Friends.DAL/WebServiceContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Json;
+using Friends.DAL.Models;
+
+namespace Friends.DAL;
+
+public class WebServiceContext
+{
+    private const string URL = "https://jsonplaceholder.typicode.com/users";
+    public IEnumerable<Friend>? Friends { get; private set; }
+
+    public async Task LoadAsync()
+    {
+        using var http = new HttpClient();
+        var data = await http.GetFromJsonAsync(URL, Friends.GetType());
+        Friends = data as IEnumerable<Friend>;
+    }
+}
